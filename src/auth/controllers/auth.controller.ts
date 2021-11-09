69674219c53e99c6f1aceab87c3f319b39fc6254
xaibe <sa30967965@gmail.com>
2021-11-09 11:00:48 +0500

    users added and authenticated

---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Controller,
+  Request,
+  Get,
+  UseGuards,
+  Post,
+  Body,
+} from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiTags,
+} from '@nestjs/swagger';
+import { LoginUserDto } from 'src/users/models/dto/Login-user.dto';
+import { Public } from '../constants';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { AuthService } from '../services/auth.service';
+@ApiTags('Auths')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @ApiCreatedResponse({ type: LoginUserDto })
+  @ApiBadRequestResponse()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @ApiCreatedResponse({ type: LoginUserDto })
+  @ApiBadRequestResponse()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req, @Body() body: LoginUserDto) {
+    return this.authService.login(req.user);
+  }
+}
